7c7dcf23c10fbd883b3b4d0e9732a0704ef9b2da
ytype <59418226+ytype@users.noreply.github.com>
2020-04-15 23:16:57 +0900

    ENH: update getElements
    - 자바스크립트 공부

--- getElements.js
+++ getElements.js
@@ -1,10 +1,10 @@
-var element_h1 = document.getElementsByTagName('h1')[0];
-console.log(element_h1.innerText);
+var element_h1 = document.getElementsByTagName('h1')[0]
+console.log(element_h1.innerText)
 
 var element_subtitle = document.getElementsByClassName('subtitle')[0]
 element_subtitle.innerText = `h1 text: ${element_h1.innerText}`
 
-var element_container = document.getElementsByClassName('container')[0]
+var h3_form = document.getElementById('form')
 
 var element_h2 = document.getElementsByName('h2')[0]
 console.log(element_h2.innerText)
@@ -14,7 +14,7 @@ function add_text(){
     var input_str = document.getElementById("input").value
     var element_h3_text = document.createTextNode(input_str)
     element_h3.appendChild(element_h3_text)
-    element_container.appendChild(element_h3)
+    h3_form.appendChild(element_h3)
 }
 
 function rm_text() {
